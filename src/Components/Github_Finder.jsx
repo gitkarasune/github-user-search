d9fbb9b5c67a94f7a450e65e788ab306dc96f60d
unknown <sunebarikara@gmail.com>
2024-11-27 11:56:56 +0100

    project completed

--- src/Components/Github_Finder.jsx
+++ src/Components/Github_Finder.jsx
@@ -1,36 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import User from './user';
+import React, { useEffect, useState } from "react";
+import User from "./user";
 
 const GithubFinder = () => {
-  const [ username, setUsername ] = useState("gitKarasune");
-  const [ userData, setUserData ] = useState(null);
-  //loading
+  const [username, setUsername] = useState("");
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleFetch = async () => {
-    // setLoading true
+    setLoading(true);
     const res = await fetch(`https://api.github.com/users/${username}`);
 
     const data = await res.json();
 
     if (data) {
       setUserData(data);
-      // setLoading false 
+      setLoading(false);
       setUsername("");
     }
 
-    console.log(data)
+    console.log(data);
+  };
+
+  if (!loading) {
+    { userData !== null ? <User user={userData} /> : null }
   }
 
   return (
     <div>
-      <div className='mb-10 flex justify-between p-2 gap-6 items-center'>
-        <input type="text" className='py-4 bg-black text-white rounded-full w-full' name='search-by-username' placeholder='Input Github Username' value={username} onChange={(e) => setUsername(e.target.value)} />
-        <button onClick={handleFetch} className='bg-black text-white rounded-full px-5 py-4'>Search</button>
-      </div>
-
+      {loading && (
+        <p className="text-5xl flex justify-center items-center h-screen font-bold">
+          Loading.....
+        </p>
+      )}
+      {!loading && (
+        <div className="mb-10 flex justify-between p-2 gap-6 items-center">
+          <input
+            type="text"
+            className="py-4 bg-black text-white rounded-full w-full"
+            name="search-by-username"
+            placeholder="Input Github Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button
+            onClick={handleFetch}
+            className="bg-black text-white rounded-full px-5 py-4"
+          >
+            Search
+          </button>
+        </div>
+      )}
       { userData !== null ? <User user={userData} /> : null }
     </div>
-  )
-}
+  );
+};
 
-export default GithubFinder
+export default GithubFinder;
--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -10,7 +10,16 @@ function User({ user }) {
     following,
     public_repos,
     bio,
-    company
+    company,
+    blog,
+    email, 
+    hireable,
+    location,
+    public_gists,
+    twitter_username,
+    updated_at,
+    html_url,
+
   } = user;
 
   const date = new Date(created_at);
@@ -33,30 +42,72 @@ function User({ user }) {
         </div>
         <div className="flex gap-2">
             <p className="font-bold">Company:</p>
-            <p>{company}</p>
+            <p>{company ? company : "No company"}</p>
         </div>
         <div className="flex gap-2">
             <p className="font-bold">Bio</p>
-            <p>{bio}</p>
+            <p>{bio ? bio : "No Bio"}</p>
+        </div>
+        <div className="flex gap-2">
+            <p className="font-bold">Find me at: </p>
+            <p>
+                <a href={`${html_url}`}>{html_url ? html_url : "No Link"}</a>
+            </p>
         </div>
       </div>
 
       <div className="flex gap-3 items-center my-4">
           <p className="font-bold">Public Repos:</p>
           <p className="bg-black w-10 h-10 rounded-full p-4 text-white flex justify-center items-center">
-            {public_repos}
+            {public_repos ? public_repos : "No Public Repos"}
           </p>
         </div>
         <div className="flex gap-3 items-center my-4">
           <p className="font-bold">Followers:</p>
           <p className="bg-black w-10 h-10 rounded-full p-4 text-white flex justify-center items-center">
-            {followers}
+            {followers ? followers : "No followers"}
           </p>
         </div>
         <div className="flex gap-3 items-center my-4">
           <p className="font-bold">Following:</p>
           <p className="bg-black w-10 h-10 rounded-full p-4 text-white flex justify-center items-center">
-            {following}
+            {following ? following : "No following"}
+          </p>
+        </div>
+        <div className="flex gap-3 items-center my-4">
+          <p className="font-bold">Blog:</p>
+          <p className="">
+            {blog ? blog : "No Blog"}
+          </p>
+        </div>
+        <div className="flex gap-3 items-center my-4">
+          <p className="font-bold">Email:</p>
+          <p >
+            {email ? email : "No Email"}
+          </p>
+        </div>
+        <div className="flex gap-3 items-center my-4">
+          <p className="font-bold">Hireable:</p>
+          <p >
+            {hireable ? hireable : "No Hireable"}
+          </p>
+        </div>
+        <div className="flex gap-3 items-center my-4">
+          <p className="font-bold">Location:</p>
+          <p >
+            {location ? location : "No Location"}
+          </p>
+        </div>
+        <div className="flex gap-3 items-center my-4">
+          <p className="font-bold">Public gists:</p>
+          <p >
+            {public_gists ? public_gists : "No Public gists"}
+          </p>
+        </div>
+        <div className="flex gap-3 items-center my-4">
+          <p className="font-bold">Twitter Username:</p>
+          <p >
+            {twitter_username ? twitter_username : "No Twitter username"}
           </p>
         </div>
     </div>
